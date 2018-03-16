a4214d0f1df8517ef36942d7d4827db723e2bd5b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-03-16 13:07:48 +1300

    Examples/experiments.

---
+++ examples/async_method.rb
@@ -0,0 +1,40 @@
+
+require_relative '../lib/async'
+
+module Async::Methods
+	def sleep(*args)
+		Async::Task.current.sleep(*args)
+	end
+
+	def async(name)
+		original_method = self.method(name)
+		
+		define_method(name) do |*args|
+			Async::Reactor.run do |task|
+				original_method.call(*args)
+			end
+		end
+	end
+
+	def await(&block)
+		block.call.wait
+	end
+end
+
+include Async::Methods
+
+async def count_chickens(area_name)
+	10.times do |i|
+		sleep rand
+		
+		puts "Found a chicken in the #{area_name}!"
+	end
+end
+
+async def count_all_chckens
+	count_chickens("garden")
+	count_chickens("house")
+	count_chickens("tree")
+end
+
+count_all_chckens
---
+++ examples/sleep_sort.rb
@@ -0,0 +1,38 @@
+
+require_relative '../lib/async'
+
+def sleep_sort(items)
+	Async::Reactor.run do |task|
+		# Where to save the sorted items:
+		sorted_items = []
+		
+		items.each do |item|
+			# Spawn an async task...
+			task.async do |nested_task|
+				# Which goes to sleep for the specified duration:
+				nested_task.sleep(item)
+				
+				# And then appends the item to the sorted array:
+				sorted_items << item
+			end
+		end
+		
+		# Wait for all children to complete.
+		task.children.each(&:wait)
+		
+		# Return the result:
+		sorted_items
+	end.wait # Wait for the entire process to complete.
+end
+
+# Calling at the top level blocks the thread:
+puts sleep_sort(5.times.collect{rand}).inspect
+
+# Calling in your own reactor allows you to control the asynchronus behaviour:
+Async::Reactor.run do |task|
+	3.times do
+		task.async do
+			puts sleep_sort(5.times.collect{rand}).inspect
+		end
+	end
+end
