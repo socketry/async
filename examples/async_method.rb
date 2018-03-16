d9e6c891c45032e45200f767f00af1c4e71d6bef
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-03-16 14:09:44 +1300

    Add `barrier!` primitive in example/experimental code.

--- examples/async_method.rb
+++ examples/async_method.rb
@@ -20,22 +20,40 @@ module Async::Methods
 	def await(&block)
 		block.call.wait
 	end
+	
+	def barrier!
+		Async::Task.current.children.each(&:wait)
+	end
 end
 
 include Async::Methods
 
 async def count_chickens(area_name)
-	10.times do |i|
+	3.times do |i|
 		sleep rand
 		
 		puts "Found a chicken in the #{area_name}!"
 	end
 end
 
+async def find_chicken(areas)
+	puts "Searching for chicken..."
+	
+	sleep rand * 5
+	
+	return areas.sample
+end
+
 async def count_all_chckens
+	# These methods all run at the same time.
 	count_chickens("garden")
 	count_chickens("house")
 	count_chickens("tree")
+	
+	# Wait for all previous async work to complete...
+	barrier!
+	
+	puts "There was a chicken in the #{find_chicken(["garden", "house", "tree"]).wait}"
 end
 
 count_all_chckens
