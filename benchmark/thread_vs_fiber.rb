a766c5209f76cdf7111759b5b1dd2f06c75d8f75
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-09-15 09:54:18 +1200

    Thread vs Fiber overheads.

---
+++ benchmark/thread_vs_fiber.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+# Copyright, 2019, by Samuel G. D. Williams. <http://www.codeotaku.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'benchmark/ips'
+
+GC.disable
+
+Benchmark.ips do |benchmark|
+	benchmark.time = 1
+	benchmark.warmup = 1
+	
+	benchmark.report("Thread.new{}") do |count|
+		while count > 0
+			Thread.new{count -= 1}.join
+		end
+	end
+	
+	benchmark.report("Fiber.new{}") do |count|
+		while count > 0
+			Fiber.new{count -= 1}.resume
+		end
+	end
+	
+	benchmark.compare!
+end
