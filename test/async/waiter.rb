d32e6629202c53a876b2f9234d9b444bf1a54754
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-11-03 22:04:21 +1300

    Rename `Async::LimitedBarrier` to `Async::Waiter`. (#196)

--- test/async/waiter.rb
+++ test/async/waiter.rb
@@ -1,23 +1,23 @@
 
-require 'async/limited_barrier'
+require 'async/waiter'
 require 'sus/fixtures/async'
 
-describe Async::LimitedBarrier do
+describe Async::Waiter do
 	include Sus::Fixtures::Async::ReactorContext
 	
-	let(:limited_barrier) {subject.new}
+	let(:waiter) {subject.new}
 	
 	it "can wait for a subset of tasks" do
 		3.times do
-			limited_barrier.async do
+			waiter.async do
 				sleep(rand * 0.01)
 			end
 		end
 		
-		done = limited_barrier.wait(2)
+		done = waiter.wait(2)
 		expect(done.size).to be == 2
 
-		done = limited_barrier.wait(1)
+		done = waiter.wait(1)
 		expect(done.size).to be == 1
 	end
 end
