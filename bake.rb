bd91daa9b0853ba0bc22a492801788a800b67bcc
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-03-28 00:57:55 +1300

    Remove async-http from external tests.

--- bake.rb
+++ bake.rb
@@ -7,7 +7,6 @@ def external
 		clone_and_test("async-io")
 		clone_and_test("async-pool", "sus")
 		clone_and_test("async-dns")
-		clone_and_test("async-http", "sus")
 		clone_and_test("falcon", "sus")
 		clone_and_test("async-rest", "sus")
 	end
