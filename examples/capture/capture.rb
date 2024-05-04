317a6ce62ec72316669d095516ea88ab459376d2
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-05-04 19:27:17 +1200

    Modernize gem.

--- examples/capture/capture.rb
+++ examples/capture/capture.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2019-2022, by Samuel Williams.
+# Copyright, 2019-2024, by Samuel Williams.
 
 require 'irb'
 require 'console'
