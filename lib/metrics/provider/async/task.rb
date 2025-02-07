b324230fc83bd93f2350dc1ea39308734544badf
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-02-07 21:01:26 +1300

    Add `async.task.finished` metric.

--- lib/metrics/provider/async/task.rb
+++ lib/metrics/provider/async/task.rb
@@ -8,10 +8,15 @@ require "metrics/provider"
 
 Metrics::Provider(Async::Task) do
 	ASYNC_TASK_SCHEDULED = Metrics.metric("async.task.scheduled", :counter, description: "The number of tasks scheduled.")
+	ASYNC_TASK_FINISHED = Metrics.metric("async.task.finished", :counter, description: "The number of tasks finished.")
 	
 	def schedule(&block)
 		ASYNC_TASK_SCHEDULED.emit(1)
 		
 		super(&block)
+	rescue => error
+		raise
+	ensure
+		ASYNC_TASK_FINISHED.emit(1)
 	end
 end
